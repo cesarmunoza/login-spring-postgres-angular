751ebe397b4d7bbcd529937f4139360beb5786fb
Cesar <cesare.munoz@gmail.com>
2024-11-18 00:11:45 -0500

    Se modificó AuthController para configurar los endpoints de login y registe. La respuesta se configura con ResponseEntity. El AuthenticationController se comunica con el AuthenticationService para acceder al token. En el login buca el usuario que está autenticado y en el register crea un nuevo registro de usuario

--- src/main/java/com/bitiot/volga3_login/app/controller/AuthController.java
+++ src/main/java/com/bitiot/volga3_login/app/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.bitiot.volga3_login.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @PostMapping(value = "login")
-    public String login(){
-        return "login from public endpoint";
+    public ResponseEntity<AuthResponse>  login(@RequestBody LoginRequest request)//Devuelve el objeto que representa toda la respuesta http, códigos de estado, header y body
+    {
+        return ResponseEntity.ok(new AuthResponse());
     }
 
     @PostMapping(value = "register")
-    public String register(){
-        return "register from public endpoint";
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(new AuthResponse());
     }
 }
