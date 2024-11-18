817143742fd8bcf2c2b6d8b48bdd280677c5723f
Cesar Munoz <cesarmu@hotmail.com>
2024-11-18 00:55:21 -0500

    Se actualizó la clase de entidad UserEntity para que extendiera UserDetails y se implementó el método getAuthorities

--- src/main/java/com/bitiot/volga3_login/app/models/UserEntity.java
+++ src/main/java/com/bitiot/volga3_login/app/models/UserEntity.java
@@ -1,5 +1,6 @@
 package com.bitiot.volga3_login.app.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -8,38 +9,96 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "users")
-public class UserEntity {
+@Table(name = "adm_users")
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Email
-    @NotBlank
-    @Size(max = 80)
-    private String email;
+    //    @NotBlank
+    private String nombre;
+
+    //    @NotBlank
+    private String apellido;
 
     @NotBlank
+    @Column(nullable = false)
     @Size(max = 30)
     private String username;
 
     @NotBlank
     private String password;
 
+    @Email
+//    @NotBlank
+    @Size(max = 80)
+    private String email;
+
+    //    @NotBlank
+    private String empresaUserId;
+
+    private Boolean userState;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+//    @NotNull
+    private Date userCreationDate;
+
+    //    @NotBlank
+    private String userIp;
+
+    private Integer notificacionDispositivoOffline;
+
+
     // Con EAGER traigo todos los roles de una vez. El Cascade con el persist solo cuando se persista un usuario y si el usuario se elimina, no elimina los roles
     @ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleEntity.class, cascade = CascadeType.PERSIST)
 
-    // Se configura la tabla intermedia entre userEntity y roleEntity y las claves foráneas: user_id para UserEntity y role_id para RoleEntity
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    // Se configura la tabla intermedia entre userEntity y roleEntity y las claves foráneas: id_user para UserEntity y id_role para RoleEntity
+    @JoinTable(
+            name = "adm_users_roles",
+            joinColumns = @JoinColumn(name = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "id_rol"))
     private Set<RoleEntity> roles = new HashSet<>();
+
+    @Transient
+    private boolean admin;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/bitiot/volga3_login/app/repositories/UserRepository.java
+++ src/main/java/com/bitiot/volga3_login/app/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.bitiot.volga3_login.app.repositories;
 
 import com.bitiot.volga3_login.app.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 
     Optional<UserEntity> findByEmail(String email);
