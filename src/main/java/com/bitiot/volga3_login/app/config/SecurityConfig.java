fe9cbbfdccb27ce848171e61b0fa11d0bc629204
Cesar Munoz <cesarmu@hotmail.com>
2024-11-19 17:24:16 -0500

    Se siguió configurando SecurityConfig con el filtro de sessionManagement y el de authenticationProvider

--- src/main/java/com/bitiot/volga3_login/app/config/SecurityConfig.java
+++ src/main/java/com/bitiot/volga3_login/app/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.bitiot.volga3_login.app.config;
 
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import com.bitiot.volga3_login.app.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authProvider = authenticationProvider;
+    }
 
     // Aquí se define la configuración de seguridad
     @Bean
@@ -29,8 +37,12 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll() // Se le permite a todos entrar a estos endpoints
                         .anyRequest().authenticated() // Cualquier otro endpoint, el usuario tiene que estar autenticado
                 )
-                .formLogin(Customizer.withDefaults())
-                ;
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
         return httpSecurity.build();
     }
 
